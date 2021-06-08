9af9c0ad0a71ee821da8d9a6be43ea8a140837e3
vic-sh <victor.shalaginov@gmail.com>
2021-06-08 10:48:00 -0400

    first commit

---
+++ pingpong3.py
@@ -0,0 +1,93 @@
+import tkinter as tk
+
+
+class GameObject(object):
+    def __init__(self, canvas, item):
+        self.canvas = canvas
+        self.item = item
+
+    def get_position(self):
+        return self.canvas.coords(self.item)
+
+    def move(self, x, y):
+        self.canvas.move(self.item, x, y)
+
+    def delete(self):
+        self.canvas.delete(self.item)
+
+
+class Ball(GameObject):
+    def __init__(self, canvas, x, y):
+        self.radius = 10
+        self.direction = [1, -1]
+        self.speed = 10
+        item = canvas.create_oval(x-self.radius, y-self.radius,
+                                  x+self.radius, y+self.radius,
+                                  fill='white')
+        super(Ball, self).__init__(canvas, item)
+
+
+class Paddle(GameObject):
+    def __init__(self, canvas, item, x, y):
+        self.width = 80
+        self.height = 10
+        self.ball = None
+        item = canvas.create_rectangle(x - self.width / 2,
+                                       y - self.height / 2,
+                                       x + self.width / 2,
+                                       y + self.height / 2,
+                                       fill='blue')
+        super(Paddle, self).__init__(canvas, item)
+
+        def set_ball(self, ball):
+            self.ball = ball
+
+        def move(self, offset):
+            coords = self.get_position()
+            width = self.canvas.winfo_width()
+            if coords[0] + offset >= 0 and coords[2] + offset <= width:
+                super(Paddle, self).move(offset, 0)
+                if self.ball is not None:
+                    self.ball.move(offset, 0)
+
+
+class Brick(GameObject):
+    def __init__(self, canvas, x, y, hits):
+        self.width = 75
+        self.height = 20
+        self.hits = hits
+        color = Brick.COLORS[hits]
+        item = canvas.create_rectangle(x - self.width / 2,
+                                       y - self.height / 2,
+                                       x + self.width / 2,
+                                       y + self.height / 2,
+                                       fill=color, tags='brick')
+        super(Brick, self).__init__(canvas, item)
+
+        def hit(self):
+            self.hits -= 1
+            if self.hits == 0:
+                self.delete()
+            else:
+                self.canvas.itemconfig(self.item,
+                                       fill=Brick.COLORS[self.hits])
+
+
+class Game(tk.Frame):
+    def __init__(self, master):
+        super(Game, self).__init__(master)
+        self.lives = 3
+        self.width = 600
+        self.height = 400
+        self.canvas = tk.Canvas(self, bg='#aaaaff',
+                                width=self.width,
+                                height=self.height)
+        self.canvas.pack()
+        self.pack()
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title('Hello, Pong')
+    game = Game(root)
+    root.mainloop()
