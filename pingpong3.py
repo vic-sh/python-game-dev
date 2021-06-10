87ccc28414f1a1b8b1259dcef02336258af3051a
vic-sh <victor.shalaginov@gmail.com>
2021-06-10 10:55:33 -0400

    changes that allow to start script

--- pingpong3.py
+++ pingpong3.py
@@ -2,6 +2,8 @@ import tkinter as tk
 
 
 class GameObject(object):
+    COLORS = {1: '#999999', 2: '#555555', 3:'#222222'}
+
     def __init__(self, canvas, item):
         self.canvas = canvas
         self.item = item
@@ -28,7 +30,7 @@ class Ball(GameObject):
 
 
 class Paddle(GameObject):
-    def __init__(self, canvas, item, x, y):
+    def __init__(self, canvas, x, y):
         self.width = 80
         self.height = 10
         self.ball = None
@@ -39,16 +41,16 @@ class Paddle(GameObject):
                                        fill='blue')
         super(Paddle, self).__init__(canvas, item)
 
-        def set_ball(self, ball):
-            self.ball = ball
+    def set_ball(self, ball):
+        self.ball = ball
 
-        def move(self, offset):
-            coords = self.get_position()
-            width = self.canvas.winfo_width()
-            if coords[0] + offset >= 0 and coords[2] + offset <= width:
-                super(Paddle, self).move(offset, 0)
-                if self.ball is not None:
-                    self.ball.move(offset, 0)
+    def move(self, offset):
+        coords = self.get_position()
+        width = self.canvas.winfo_width()
+        if coords[0] + offset >= 0 and coords[2] + offset <= width:
+            super(Paddle, self).move(offset, 0)
+            if self.ball is not None:
+                self.ball.move(offset, 0)
 
 
 class Brick(GameObject):
@@ -84,6 +86,53 @@ class Game(tk.Frame):
                                 height=self.height)
         self.canvas.pack()
         self.pack()
+        self.items = {}
+        self.ball = None
+        self.paddle = Paddle(self.canvas, self.width/2, 326)
+        self.items[self.paddle.item] = self.paddle
+        for x in range(5, self.width - 5, 75):
+            self.add_brick(x + 37.5, 50, 2)
+            self.add_brick(x + 37.5, 70, 1)
+            self.add_brick(x + 37.5, 90, 1)
+
+            self.hud = None
+            self.setup_game()
+            self.canvas.focus_set()
+            self.canvas.bind('<Left>',
+                             lambda _: self.paddle.move(-10))
+            self.canvas.bind('<Right>',
+                             lambda _: self.paddle.move(10))
+
+    def setup_game(self):
+        self.add_ball()
+        self.update_lives_text()
+        self.text = self.draw_text(300, 200,
+                                   'Press Space to start')
+        self.canvas.bind('<space>',
+                         lambda _: self.start_game())
+
+    def add_ball(self):
+        if self.ball is not None:
+            self.ball.delete()
+        paddle_coords = self.paddle.get_position()
+        x = (paddle_coords[0] + paddle_coords[2]) * 0.5
+        self.ball = Ball(self.canvas, x, 310)
+        self.paddle.set_ball(self.ball)
+
+    def add_brick(self, x, y, hits):
+        brick = Brick(self.canvas, x, y, hits)
+        self.items[brick.item] = brick
+
+    def draw_text(self, x, y, text, size='40'):
+        font = ('Helvetica', size)
+        return self.canvas.create_text(x, y, text=text, font=font)
+
+    def update_lives_text(self):
+        text = 'Lives: %s' % self.lives
+        if self.hud is None:
+            self.hud = self.draw_text(50, 20, text, 15)
+        else:
+            self.canvas.itemconfig(self.hud, text=text)
 
 
 if __name__ == '__main__':
